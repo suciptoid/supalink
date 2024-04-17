2c486dfb32705a6c83d73ac6e35ed23efab3058a
Sucipto <sucipto@pm.me>
2024-04-17 22:57:08 +0700

    appheader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -15,8 +15,9 @@ const logout = async () => {
 
 <template>
   <UContainer>
-    <div class="flex items-center md:justify-between justify-center">
-      <div class="flex items-center">
+    <div class="flex items-center md:justify-between justify-center py-3">
+      <div class="text-lg font-bold">SupaLink</div>
+      <div class="flex items-center gap-3">
         <ColorScheme
           ><USelect
             v-model="$colorMode.preference"
